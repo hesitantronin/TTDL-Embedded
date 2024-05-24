fe283ff9c590162c929ff2c270f0cfe32752b663
Ezra <124691102+Ezkrraa@users.noreply.github.com>
2024-05-24 12:10:46 +0200

    Updated comments and explained the use of every import

--- main/main.c
+++ main/main.c
@@ -1,17 +1,19 @@
+// for printf
 #include <stdio.h>
-
+// for LEDs and other GPIOs
 #include "driver/gpio.h"
-#include "esp_log.h"
+// RTOS, handles tasks and delays
 #include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "sdkconfig.h"
-#include "string.h"
-// #include "taskdelayuntil"
+// RTC (Real Time Clock) starts ticking at startup
+#include "rtc.h"
 
-// #include "wroom_log.h"
+// seemingly unused imports:
+// #include "sdkconfig.h"
+// #include "freertos/task.h"
 
-// 500 ms
+// 100 ms
 #define PAUSE_TIME 100
+
 // light is at GPIO 2
 #define BLINK_GPIO 2
 
@@ -21,19 +23,20 @@ static uint8_t s_led_state = 0;
 void configure_led(void)
 {
     esp_rom_gpio_pad_select_gpio(BLINK_GPIO);
-    /* Set the GPIO as a push/pull output */
+    // Set the GPIO as a push/pull output
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
 }
 
 void blink_led(void)
 {
+    uint64_t startTime = esp_rtc_get_time_us();
     int blinks = 0;
     while (true)
     {
         // switch LED state
         s_led_state = !s_led_state;
         // print over serial
-        printf("Turning the LED %s! GPIO:%d, Blinked % 4i times.\n", s_led_state == false ? "  ON" : " OFF", BLINK_GPIO, blinks);
+        printf("Turning the LED %s! GPIO:%d, Blinked % 4i times. Time passed: %f\n", s_led_state == false ? "  ON" : " OFF", BLINK_GPIO, blinks, ((float)esp_rtc_get_time_us() - startTime) / 1000000);
         // set to new state
         gpio_set_level(BLINK_GPIO, s_led_state);
         blinks++;
@@ -44,7 +47,6 @@ void blink_led(void)
 
 void app_main(void)
 {
-    /* Configure the peripheral according to the LED type */
     configure_led();
     blink_led();
 }
